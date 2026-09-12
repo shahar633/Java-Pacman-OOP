efabebad55975174b30956275675962f509d6e7f
Shahar Cohen <shaharcohen8@mail.tau.ac.il>
2026-09-12 19:48:41 +0300

    Update Files

--- src/inventory/Collectible.java
+++
@@ -1,9 +0,0 @@
-package il.ac.tau.cs.software1.inventory;
-
-public interface Collectible {
-	// Returns the weight of the collectible
-	public float getWeight();
-	
-	// Returns the price of the collectible
-	public float getPrice();
-}
--- src/sokoban/SokobanAbstractMove.java
+++
@@ -1,9 +0,0 @@
-package il.ac.tau.cs.software1.sokoban;
-
-public abstract class SokobanAbstractMove {
-	protected SokobanDirection dir;
-	
-	public abstract boolean isValid(SokobanPlan b);
-	public abstract SokobanPlan apply(SokobanPlan b);
-	public SokobanAbstractMove(SokobanDirection dir) { this.dir = dir; }
-}
